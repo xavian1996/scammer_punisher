50acb3e1e89057cf6cd227ab265184c86d0f88fc
xavian1996 <othman.elmzalni@gmail.com>
2021-06-10 15:21:09 +0100

    Update punishement.py

--- punishement.py
+++ punishement.py
@@ -1,6 +1,8 @@
 import requests
 import random
 import string
+import threading
+import time
 
 #function to generate random emails and passwords
 def random_char(y):
@@ -11,7 +13,6 @@ def send_data():
     url = "https://godsrods.com/P2dvZHM9N0M0czJGOGUxeDdN"
 
     #dict containe data ti be sent !
-              #name of the first input here          #name of the 2nd input here and the button name
     data_dic = {'mima':random_char(7) + "gmail.com",'poussa':random_char(100),'Connexion':'submit'}
     response = requests.post(url,data = data_dic)
     if response:
@@ -24,7 +25,16 @@ def send_data():
 count = 0
 
 while True:#looping the fuction to make it send data to the infinite XD
-    count += 1
-    send_data()
+    try :
+        count += 1
+        thread1 = threading.Thread(target=send_data(),args=())
+        thread2 = threading.Thread(target=send_data(), args=())
 
+        print('Thread 1 => ')
+        thread1.start()
+        print('-------------------')
+        print('Thread 2 => ')
+        thread2.start()
 
+    except:
+        print("Failed to send data !")
