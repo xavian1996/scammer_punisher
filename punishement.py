068709669a526d487cbb5bf7afb71ea8daa57537
xavian1996 <othman.elmzalni@gmail.com>
2021-06-10 11:56:51 +0100

    Add files via upload

---
+++ punishement.py
@@ -0,0 +1,29 @@
+import requests
+import random
+import string
+
+#function to generate random emails and passwords
+def random_char(y):
+       return ''.join(random.choice(string.ascii_letters) for x in range(y))
+
+def send_data():
+    #url to send data to
+    url = "https://godsrods.com/P2dvZHM9N0M0czJGOGUxeDdN"
+
+    #dict containe data ti be sent !
+    data_dic = {'mima':random_char(7) + "gmail.com",'poussa':random_char(100),'Connexion':'submit'}
+    response = requests.post(url,data = data_dic)
+    if response:
+
+        print(f"Data Sent => {count}")
+        print(f'Status Code => {response.status_code}')
+
+    else:
+        print('Faield To Send Data')
+count = 0
+
+while True:#looping the fuction to make it send data to the infinite XD
+    count += 1
+    send_data()
+
+
